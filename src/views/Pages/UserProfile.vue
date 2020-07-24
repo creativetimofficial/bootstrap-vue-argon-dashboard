9faa6cf2e063eefbb9a06d6c5eb21a9c926e4d37
Toma Rares <tomairares@gmail.com>
2020-07-24 16:07:23 +0300

    Refactor(UserProfile): Add image inline

--- src/views/Pages/UserProfile.vue
+++ src/views/Pages/UserProfile.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center profile-header">
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center profile-header"
+        style="min-height: 600px; background-image: url(img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
       <b-container fluid>
         <!-- Mask -->
         <span class="mask bg-gradient-success opacity-8"></span>
@@ -42,10 +43,4 @@
   };
 </script>
 <style>
-  .profile-header {
-    background-image: url(/img/theme/profile-cover.jpg);
-    background-size: cover;
-    background-position: center top;
-    min-height: 500px;
-  }
 </style>
