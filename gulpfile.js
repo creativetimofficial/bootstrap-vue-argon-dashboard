248355cfe658921000b8819f7feee4b93ac408df
Toma Rares <tomairares@gmail.com>
2020-07-24 14:04:56 +0300

    Feat(gulpfile): Add gulpfile and dependencies for licenses

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+  // Gulp and plugins
+const
+  gulp  = require('gulp'),
+  gap = require('gulp-append-prepend'),
+  product_name = "BootstrapVue Argon Dashboard",
+  product_version = "1.0.0",
+  year = "2020",
+  github_MIT_link = "https://github.com/creativetimofficial/bootstrap-vue-argon-dashboard/blob/master/LICENSE.md",
+  product_link = "https://www.creative-tim.com/product/bootstrap-vue-argon-dashboard",
+  product_copyright_template = `/*!
+  ​
+  =========================================================
+  * `+ product_name +` - v`+ product_version +`
+  =========================================================
+
+  * Product Page: `+ product_link +`
+  * Copyright `+ year +` Creative Tim (https://www.creative-tim.com)
+
+  * Coded by Creative Tim
+
+  =========================================================
+
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    ​
+  */`
+
+gulp.task("licenses", async function() {
+  // this is to add Creative Tim licenses in the production & dev mode for the minified js
+  gulp.src(["dist/js/chunk-vendors*.js", "dist/js/app*.js"], { base: "./" })
+  .pipe(gap.prependText(product_copyright_template))
+  .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production & dev mode for the minified html
+  gulp.src("dist/index.html", { base: "./" })
+  .pipe(gap.prependText(product_copyright_template))
+  .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production & dev mode for the minified css
+  gulp.src(["dist/css/chunk-vendors*.css", "dist/app*.css"], { base: "./" })
+  .pipe(gap.prependText(product_copyright_template))
+  .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
