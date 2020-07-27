6ab35f0620c7f057ea378dff68fac404f72ac2d3
Toma Rares <tomairares@gmail.com>
2020-07-27 16:36:37 +0300

    Feat: Add breadcrumb

---
+++ src/components/Breadcrumb/BreadcrumbItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-breadcrumb-item :active="active">
+    <slot></slot>
+  </b-breadcrumb-item>
+</template>
+<script>
+export default {
+  name: 'breadcrumb-item',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+      description: 'Whether breadcrumb item is active'
+    }
+  }
+};
+</script>
+<style></style>
