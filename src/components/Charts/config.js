6489e1f3b8075edeaac04926bdb920f6747c48e3
Maisam Afshar <maisam.afshar@gmail.com>
2022-12-14 12:58:25 +0430

    update date & version

--- src/components/Charts/config.js
+++ src/components/Charts/config.js
@@ -1,35 +1,35 @@
 import { parseOptions } from "@/components/Charts/optionHelpers";
-import Chart from 'chart.js'
+import Chart from "chart.js";
 
 export const Charts = {
-  mode: 'light',//(themeMode) ? themeMode : 'light';
+  mode: "light", //(themeMode) ? themeMode : 'light';
   fonts: {
-    base: 'Open Sans'
+    base: "Open Sans"
   },
   colors: {
     gray: {
-      100: '#f6f9fc',
-      200: '#e9ecef',
-      300: '#dee2e6',
-      400: '#ced4da',
-      500: '#adb5bd',
-      600: '#8898aa',
-      700: '#525f7f',
-      800: '#32325d',
-      900: '#212529'
+      100: "#f6f9fc",
+      200: "#e9ecef",
+      300: "#dee2e6",
+      400: "#ced4da",
+      500: "#adb5bd",
+      600: "#8898aa",
+      700: "#525f7f",
+      800: "#32325d",
+      900: "#212529"
     },
     theme: {
-      'default': '#172b4d',
-      'primary': '#5e72e4',
-      'secondary': '#f4f5f7',
-      'info': '#11cdef',
-      'success': '#2dce89',
-      'danger': '#f5365c',
-      'warning': '#fb6340'
+      default: "#172b4d",
+      primary: "#5e72e4",
+      secondary: "#f4f5f7",
+      info: "#11cdef",
+      success: "#2dce89",
+      danger: "#f5365c",
+      warning: "#fb6340"
     },
-    black: '#12263F',
-    white: '#FFFFFF',
-    transparent: 'transparent',
+    black: "#12263F",
+    white: "#FFFFFF",
+    transparent: "transparent"
   }
 };
 
@@ -41,8 +41,8 @@ function chartOptions() {
       global: {
         responsive: true,
         maintainAspectRatio: false,
-        defaultColor: (mode === 'dark') ? colors.gray[700] : colors.gray[600],
-        defaultFontColor: (mode === 'dark') ? colors.gray[700] : colors.gray[600],
+        defaultColor: mode === "dark" ? colors.gray[700] : colors.gray[600],
+        defaultFontColor: mode === "dark" ? colors.gray[700] : colors.gray[600],
         defaultFontFamily: fonts.base,
         defaultFontSize: 13,
         layout: {
@@ -50,7 +50,7 @@ function chartOptions() {
         },
         legend: {
           display: false,
-          position: 'bottom',
+          position: "bottom",
           labels: {
             usePointStyle: true,
             padding: 16
@@ -59,51 +59,54 @@ function chartOptions() {
         elements: {
           point: {
             radius: 0,
-            backgroundColor: colors.theme['primary']
+            backgroundColor: colors.theme["primary"]
           },
           line: {
-            tension: .4,
+            tension: 0.4,
             borderWidth: 4,
-            borderColor: colors.theme['primary'],
+            borderColor: colors.theme["primary"],
             backgroundColor: colors.transparent,
-            borderCapStyle: 'rounded'
+            borderCapStyle: "rounded"
           },
           rectangle: {
-            backgroundColor: colors.theme['warning']
+            backgroundColor: colors.theme["warning"]
           },
           arc: {
-            backgroundColor: colors.theme['primary'],
-            borderColor: (mode == 'dark') ? colors.gray[800] : colors.white,
+            backgroundColor: colors.theme["primary"],
+            borderColor: mode == "dark" ? colors.gray[800] : colors.white,
             borderWidth: 4
           }
         },
         tooltips: {
           enabled: true,
-          mode: 'index',
-          intersect: false,
+          mode: "index",
+          intersect: false
         }
       },
       pie: {
         tooltips: {
-          mode: 'point'
+          mode: "point"
         }
       },
       doughnut: {
         tooltips: {
-          mode: 'point'
+          mode: "point"
         },
         cutoutPercentage: 83,
-        legendCallback: function (chart) {
+        legendCallback: function(chart) {
           let data = chart.data;
-          let content = '';
+          let content = "";
 
-          data.labels.forEach(function (label, index) {
+          data.labels.forEach(function(label, index) {
             let bgColor = data.datasets[0].backgroundColor[index];
 
             content += '<span class="chart-legend-item">';
-            content += '<i class="chart-legend-indicator" style="background-color: ' + bgColor + '"></i>';
+            content +=
+              '<i class="chart-legend-indicator" style="background-color: ' +
+              bgColor +
+              '"></i>';
             content += label;
-            content += '</span>';
+            content += "</span>";
           });
 
           return content;
@@ -113,37 +116,37 @@ function chartOptions() {
   };
 
   // yAxes
-  Chart.scaleService.updateScaleDefaults('linear', {
+  Chart.scaleService.updateScaleDefaults("linear", {
     gridLines: {
       borderDash: [2],
       borderDashOffset: [2],
-      color: (mode === 'dark') ? colors.gray[900] : colors.gray[200],
+      color: mode === "dark" ? colors.gray[900] : colors.gray[200],
       drawBorder: false,
       drawTicks: true,
       zeroLineWidth: 1,
-      zeroLineColor: (mode === 'dark') ? colors.gray[900] : colors.gray[200],
+      zeroLineColor: mode === "dark" ? colors.gray[900] : colors.gray[200],
       zeroLineBorderDash: [2],
       zeroLineBorderDashOffset: [2]
     },
     ticks: {
       beginAtZero: true,
       padding: 10,
-      callback: function (value) {
+      callback: function(value) {
         if (!(value % 10)) {
-          return value
+          return value;
         }
       }
     }
   });
 
   // xAxes
-  Chart.scaleService.updateScaleDefaults('category', {
+  Chart.scaleService.updateScaleDefaults("category", {
     gridLines: {
       drawBorder: false,
       drawOnChartArea: false,
       drawTicks: false,
       lineWidth: 1,
-      zeroLineWidth: 1,
+      zeroLineWidth: 1
     },
     ticks: {
       padding: 20
@@ -152,16 +155,16 @@ function chartOptions() {
   });
 
   return options;
-};
+}
 
-let initialized = false
+let initialized = false;
 
 export function initGlobalOptions() {
   if (initialized) {
-    return
+    return;
   }
   parseOptions(Chart, chartOptions());
-  initialized = true
+  initialized = true;
 }
 
 export const basicOptions = {
@@ -172,58 +175,60 @@ export const basicOptions = {
   responsive: true
 };
 export let blueChartOptions = {
-  scales: {
-    yAxes: [{
-      gridLines: {
-        color: Charts.colors.gray[700],
-        zeroLineColor: Charts.colors.gray[700]
+  dataset: {
+    dataset: [
+      {
+        gridLines: {
+          color: Charts.colors.gray[700],
+          zeroLineColor: Charts.colors.gray[700]
+        }
       }
-    }]
+    ]
   }
 };
 
 export let lineChartOptionsBlue = {
   ...basicOptions,
   tooltips: {
-    backgroundColor: '#f5f5f5',
-    titleFontColor: '#333',
-    bodyFontColor: '#666',
+    backgroundColor: "#f5f5f5",
+    titleFontColor: "#333",
+    bodyFontColor: "#666",
     bodySpacing: 4,
     xPadding: 12,
-    mode: 'nearest',
+    mode: "nearest",
     intersect: 0,
-    position: 'nearest'
+    position: "nearest"
   },
   responsive: true,
   scales: {
-    yAxes: [
+    dataset: [
       {
         barPercentage: 1.6,
         gridLines: {
           drawBorder: false,
-          color: 'rgba(29,140,248,0.0)',
-          zeroLineColor: 'transparent'
+          color: "rgba(29,140,248,0.0)",
+          zeroLineColor: "transparent"
         },
         ticks: {
           suggestedMin: 60,
           suggestedMax: 125,
           padding: 20,
-          fontColor: '#9e9e9e'
+          fontColor: "#9e9e9e"
         }
       }
     ],
 
-    xAxes: [
+    dataset: [
       {
         barPercentage: 1.6,
         gridLines: {
           drawBorder: false,
-          color: 'rgba(29,140,248,0.1)',
-          zeroLineColor: 'transparent'
+          color: "rgba(29,140,248,0.1)",
+          zeroLineColor: "transparent"
         },
         ticks: {
           padding: 20,
-          fontColor: '#9e9e9e'
+          fontColor: "#9e9e9e"
         }
       }
     ]
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* BootstrapVue Argon Dashboard - v1.0.0
+* BootstrapVue Argon Dashboard - v1.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/bootstrap-vue-argon-dashboard
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
 * Coded by www.creative-tim.com
 
@@ -14,18 +14,18 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Vue from 'vue';
-import DashboardPlugin from './plugins/dashboard-plugin';
-import App from './App.vue';
+import Vue from "vue";
+import DashboardPlugin from "./plugins/dashboard-plugin";
+import App from "./App.vue";
 
 // router setup
-import router from './routes/router';
+import router from "./routes/router";
 // plugin setup
 Vue.use(DashboardPlugin);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App),
   router
 });
